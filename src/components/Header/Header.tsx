e0d4b2fe5ffbf20a8a58d1c6d802c6cca22f958e
Vishwa Perera <vperera@mrscompany.com>
2024-12-25 17:14:17 -0500

    added nav right container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,26 @@ import * as classes from "./Header.module.scss"
 import { Logo } from "@/components"
 
 export default () => {
+  const sections = [
+    { link: "/#about", text: "About" },
+    { link: "/#experience", text: "Experience" },
+    { link: "/#solutions", text: "Solutions" },
+    { link: "/#contact", text: "Contact" },
+  ]
+
+  const listItems = sections.map((x) => (
+    <li className={classes.navLink} key={x.link}>
+      <a href={x.link}>{x.text}</a>
+    </li>
+  ))
+
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
         <Logo />
+        <div className={classes.navRightContainer}>
+          <ol className={classes.navLinksContainer}>{listItems}</ol>
+        </div>
       </nav>
     </header>
   )
