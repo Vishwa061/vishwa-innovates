96abd8dff85ba814f09670b6397a91138589ddfa
Vishwa Perera <pereravishwa061@gmail.com>
2025-12-17 21:02:19 -0500

    u

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -7,19 +7,6 @@ export default () => {
       <span className={classes.hi}>Hi, my name is</span>
       <span className={classes.name}>Vishwa Perera.</span>
       <span className={classes.job}>I'm a Software Engineer.</span>
-      <p className={classes.desc}>
-        I specialize in simplifying complex challenges and developing innovative
-        solutions that deliver a meaningful impact. In my current role at{" "}
-        <a
-          href="https://www.citi.com"
-          target="_blank"
-          className={classes.company}
-        >
-          Citi
-        </a>
-        , I build, maintain, and enhance data pipelines & microservices in the
-        equities sector.
-      </p>
     </section>
   )
 }
