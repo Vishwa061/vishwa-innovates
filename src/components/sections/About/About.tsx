c57d76618b840f753f1bf638c21e4f9cf45ec593
Vishwa Perera <pereravishwa061@gmail.com>
2025-12-17 22:11:31 -0500

    Add motto and footer content

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -7,6 +7,7 @@ export default () => {
       <span className={classes.hi}>Hi, my name is</span>
       <span className={classes.name}>Vishwa Perera.</span>
       <span className={classes.job}>I'm a Software Engineer.</span>
+      <span className={classes.motto}>Simplicity Is Innovation</span>
     </section>
   )
 }
