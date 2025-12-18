f984016e8041ad1c24b24046bb783dde287a99b8
Vishwa Perera <pereravishwa061@gmail.com>
2025-12-17 20:48:13 -0500

    Adjust experience titles per review

--- src/components/sections/Experience/Experience.tsx
+++ src/components/sections/Experience/Experience.tsx
@@ -12,23 +12,28 @@ export default () => {
       company: "Citi",
       logo: citiLogo,
       location: "Mississauga, Ontario, Canada",
-      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                  lacus tortor, eleifend posuere tellus nec, auctor suscipit tellus.
-                  Phasellus imperdiet pharetra sem, vitae dignissim neque posuere nec.
-                  Duis feugiat gravida metus, quis placerat ex aliquet id.`,
-      tags: ["Angular", "Python", "Java"],
+      desc: [
+        "Maintained and developed the front end of a data quality and controls app using Citi’s standard UI library (Angular/TypeScript)",
+        "Built the front end for an innovative data quality app which used generative AI for rule creation",
+        "Revamped the UI & constructed the API of a team hackathon project which was approved for further development",
+      ],
+      tags: ["Angular", "TypeScript", "Generative AI", "UI"],
     },
     {
       roleInfo: "Full Stack Engineer",
-      roleInterval: "June 2021 — June 2023",
+      roleInterval: "June 2021 – Aug 2022; Jan 2023 – June 2023",
       company: "MRS Company",
       logo: mrsLogo,
       location: "Toronto, Ontario, Canada",
-      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                  lacus tortor, eleifend posuere tellus nec, auctor suscipit tellus.
-                  Phasellus imperdiet pharetra sem, vitae dignissim neque posuere nec.
-                  Duis feugiat gravida metus, quis placerat ex aliquet id.`,
-      tags: ["React", ".NET", "SQL"],
+      desc: [
+        "Routinely developed complex features on the front end with React/TypeScript and on the back end with .NET Core, C#, Entity Framework (ORM)",
+        "Experience using libraries such as Redux, Hangfire and SignalR",
+        "Assisted in Azure DevOps setup for CI/CD and bug tracking, closed 100+ bugs",
+        "Worked with a team of experienced developers to design a system dealing with large datasets and concurrency",
+        "Introduced best practices and methodologies that improved product definition and provided customization of applications to meet client needs",
+        "Wrote SQL stored procedures to optimize client app core feature performance from hours to seconds",
+      ],
+      tags: ["React", "TypeScript", ".NET Core", "Entity Framework", "Azure DevOps", "SQL"],
     },
   ]
 
